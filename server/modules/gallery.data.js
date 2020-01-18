65fae4c12707f3b782d7950012ed708acec06942
Meghan Gunderson <megundersonmn@gmail.com>
2020-01-18 17:41:39 -0600

    set up state and set up GET request on app.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,11 +1,11 @@
 const galleryItems = [
     { //id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0,
-    id: 1, path: 'images/Kayaking_Seattle.JPG', description: 'My friend Chris and I kayaking on the Puget Sound.', likes: 0,
-    id: 2, path: 'images/AlfiePuppy.JPG', description: 'I got a puppy named Alfie in the summer of 2018.', likes: 0,
-    id: 3, path: 'images/Big Bend_Camping.JPG', description: 'Sunset over the Rio Grande in Big Bend National Park.', likes: 0,
-    id: 4, path: 'images/JUNE.jpg', description: 'The promo shot for JUNE, one of my favorite shows that I have worked on.', likes: 0,
-    id: 5, path: 'images/Alfie_MEG.JPG', description: 'Alfie and me!', likes: 0,
-    id: 6, path: 'images/London.jpg', description: 'On the West Bank in London.', likes: 0}
+    id: 1, title: 'kayaking', path: 'images/Kayaking_Seattle.JPG', description: 'My friend Chris and I kayaking on the Puget Sound.', likes: 0,
+    id: 2, title: 'puppy', path: 'images/AlfiePuppy.JPG', description: 'I got a puppy named Alfie in the summer of 2018.', likes: 0,
+    id: 3, title: 'bigBend', path: 'images/Big Bend_Camping.JPG', description: 'Sunset over the Rio Grande in Big Bend National Park.', likes: 0,
+    id: 4, title: 'june', path: 'images/JUNE.jpg', description: 'The promo shot for JUNE, one of my favorite shows that I have worked on.', likes: 0,
+    id: 5, title: 'alfieAndMe', path: 'images/Alfie_MEG.JPG', description: 'Alfie and me!', likes: 0,
+    id: 6, title: 'london', path: 'images/London.jpg', description: 'On the West Bank in London.', likes: 0}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
 
 
 class App extends Component {
+  //Create state
+  state = {
+    galleryList: [],
+    newPic: {
+      description: '',
+      likes: ''
+    }   
+  }
+
+
+  //GET data 
+  //Use axios to retrieve (GET) data from to /gallery and store it in App.js. 
+  getPictures = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      //with axios our rows are on .data of the response```
+      console.log(response.data);
+      //save in state!
+      this.setState({
+        galleryList : response.data
+      })
+    }).catch((err) => {
+      alert(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +41,9 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
+        <GalleryList />
+
       </div>
     );
   }

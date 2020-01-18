134a3df779eaba25ab026f380e23ac0bcb32e184
Meghan Gunderson <megundersonmn@gmail.com>
2020-01-18 16:45:10 -0600

    fixed ids and typos for pics in gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,9 +3,9 @@ const galleryItems = [
     id: 1, path: 'images/Kayaking_Seattle.JPG', description: 'My friend Chris and I kayaking on the Puget Sound.', likes: 0,
     id: 2, path: 'images/AlfiePuppy.JPG', description: 'I got a puppy named Alfie in the summer of 2018.', likes: 0,
     id: 3, path: 'images/Big Bend_Camping.JPG', description: 'Sunset over the Rio Grande in Big Bend National Park.', likes: 0,
-    id: 3, path: 'images/JUNE.jpg', description: 'The promo shot for JUNE, one of my favorite shows that I have worked on.', likes: 0,
-    id: 3, path: 'images/Alfie_MEG.JPG', description: 'Alfie and me!', likes: 0,
-    id: 3, path: 'images/London.jpg', description: 'On the West Bank in London.', likes: 0,}
+    id: 4, path: 'images/JUNE.jpg', description: 'The promo shot for JUNE, one of my favorite shows that I have worked on.', likes: 0,
+    id: 5, path: 'images/Alfie_MEG.JPG', description: 'Alfie and me!', likes: 0,
+    id: 6, path: 'images/London.jpg', description: 'On the West Bank in London.', likes: 0}
 ];
 
 module.exports = galleryItems;
